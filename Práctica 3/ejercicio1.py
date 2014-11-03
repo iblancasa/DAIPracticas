fdee685d5e1f11518458aaf5ad21e4b1144f5fa5
Israel Blancas <iblancasa@gmail.com>
2014-11-03 11:12:05 +0100

    Casi acabado el ejercicio 1

--- Práctica 3/ejercicio1.py
+++ Práctica 3/ejercicio1.py
@@ -6,10 +6,13 @@ from web import form
 urls = ('/', 'index')
 app = web.application(urls, globals())
 
+vemail = form.regexp(r".*@.*", "Debe ser un email valido")
+vvisa = form.regexp(r"[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}", "Debe ser una visa valida")
+
 myform = form.Form(
-    form.Textbox('nombre',description="Nombre"),
+    form.Textbox('nombre',form.Validator("El nombre no puede estar vacio", lambda i: i !=""),description="Nombre"),
 	form.Textbox('apellidos',description="Apellidos"),
-    form.Textbox('correo',description="Correo"),
+    form.Textbox('correo',vemail,description="Correo"),
     form.Dropdown('dianacimiento',[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'),
      (9, '9'), (10, '10'), (11, '11'), (12, '12'), (13, '13'), (14, '14'), (15, '15'), (16, '16'), (17, '17'), (18, '18'), (19, '19'), (20, '20'), 
      (21, '21'), (22, '22'), (23, '23'), (24, '24'), (25, '25'), (26, '26'), (27, '27'), (28, '28'), (29, '29'), (30, '30'), (31, '31')],description="Dia de nacimiento"),
@@ -18,14 +21,14 @@ myform = form.Form(
     form.Dropdown('anonacimiento',[(1993,'1993'),(1992,'1992'),(1991,'1991')],description="Ano nacimiento"),
     form.Textarea('direccion',description="Direccion"),
     form.Password('password',description="Password"),
-    form.Textbox('visa',description="Numero visa"),
-    form.Password('verificacion',description="Verificacion"),
+    form.Password('password2',description="Verificacion"),
+    form.Textbox('visa',vvisa,description="Numero visa"),
     form.Radio('formapago',['Contra reembolso','Tarjeta Visa']),
     form.Checkbox('aceptacion', form.Validator("Acepta las clausulas", lambda i: i == 'true'), value='true'),
-    form.Button('submit',type="submit", description="Enviar")
+    form.Button('submit',type="submit", description="Enviar"),
+    validators = [
+        form.Validator("Las pass no coinciden", lambda i: i.password == i.password2)]
 )
-
-
 
 
 class index:        
