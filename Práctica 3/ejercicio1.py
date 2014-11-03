7ba745f088efd66718eecdad421b79a946271023
Israel Blancas <iblancasa@gmail.com>
2014-11-03 10:32:12 +0100

    Arreglados algunos problemas con la creación del formulario

--- Práctica 3/ejercicio1.py
+++ Práctica 3/ejercicio1.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 import web
-        
-urls = (
-    '/(.*)', 'index'
-)
+from web import form
+
+urls = ('/', 'index')
 app = web.application(urls, globals())
 
-login = form.Form(
+myform = form.Form(
     form.Textbox('nombre',description="Nombre"),
-    form.Textbox('apellidos',description="Apellidos"),
+	form.Textbox('apellidos',description="Apellidos"),
     form.Textbox('correo',description="Correo"),
-    form.Dropdown('dianacimiento',description="Dia de nacimiento",[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'),
+    form.Dropdown('dianacimiento',[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'),
      (9, '9'), (10, '10'), (11, '11'), (12, '12'), (13, '13'), (14, '14'), (15, '15'), (16, '16'), (17, '17'), (18, '18'), (19, '19'), (20, '20'), 
-     (21, '21'), (22, '22'), (23, '23'), (24, '24'), (25, '25'), (26, '26'), (27, '27'), (28, '28'), (29, '29'), (30, '30'), (31, '31')]),
-    form.Dropdown('mesnacimiento',description="Mes de nacimiento",[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'),
-     (9, '9'), (10, '10'), (11, '11'), (12, '12')]),
-    form.Dropdown('anonacimiento',description="Ano nacimiento",[(1993,'1993'),(1992,'1992'),(1991,'1991')]),
+     (21, '21'), (22, '22'), (23, '23'), (24, '24'), (25, '25'), (26, '26'), (27, '27'), (28, '28'), (29, '29'), (30, '30'), (31, '31')],description="Dia de nacimiento"),
+     form.Dropdown('mesnacimiento',[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'), (8, '8'),
+     (9, '9'), (10, '10'), (11, '11'), (12, '12')],description="Mes de nacimiento"),
+    form.Dropdown('anonacimiento',[(1993,'1993'),(1992,'1992'),(1991,'1991')],description="Ano nacimiento"),
     form.Textarea('direccion',description="Direccion"),
     form.Password('password',description="Password"),
     form.Textbox('visa',description="Numero visa"),
     form.Password('verificacion',description="Verificacion"),
     form.Radio('formapago',['Contra reembolso','Tarjeta Visa']),
-    form.Checkbox('aceptacion'),
+    form.Checkbox('aceptacion', form.Validator("Acepta las clausulas", lambda i: i == 'true'), value='true'),
     form.Button('submit',type="submit", description="Enviar")
 )
 
@@ -30,12 +29,17 @@ login = form.Form(
 
 
 class index:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello, ' + name + '!'
+    def GET(self):
+    	form = myform()
+    	return "<html><body><form name=\"main\" method=\"post\"> "+form.render()+"</form></body></html>"
+    
 
-
+    def POST(self): 
+	    form = myform() 
+	    if not form.validates(): 
+	    	return "<html><body><form name=\"main\" method=\"post\"> "+form.render()+"</form></body></html>"
+	    else:   
+	    	return "hello";
 
 if __name__ == "__main__":
     app.run()
