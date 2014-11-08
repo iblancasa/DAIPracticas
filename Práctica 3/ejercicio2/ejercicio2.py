bad1a58a160b3dc372482c7ece2cd747729d8ca5
Israel Blancas <iblancasa@gmail.com>
2014-11-08 11:40:45 +0100

    Terminado el ejercicio 2

--- Práctica 3/ejercicio2/ejercicio2.py
+++ Práctica 3/ejercicio2/ejercicio2.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*- 
 
 import web
+from web import form
 from web.contrib.template import render_mako
 
 urls = (
@@ -9,16 +10,35 @@ urls = (
         )
 
 app = web.application(urls, globals(), autoreload=True)
+
+
 render = render_mako(
         directories=['templates'],
         input_encoding='utf-8',
         output_encoding='utf-8',
         )
 
+myform = form.Form(
+        form.Textbox('user', description="User"),
+        form.Password('password',description="Pass"),
+        form.Button('Enviar',type="submit")
+    )
+
+
 class index:
     def GET(self):
-    	nombre = "paco"
-        return render.index(name=nombre)
+        form = myform()
+        cabecera = "<form name=\"main\" method=\"post\"> "+form.render()+"</form>"
+        return render.index(form=cabecera)
+
+
+    def POST(self):
+        form = myform()
+        if not form.validates() or form.d.user!="dai" or form.d.password !="dai": 
+            cabecera = "<form name=\"main\" method=\"post\"> "+form.render()+"</form><p>EL LOGIN FALLO</p>"
+        else:
+            cabecera = "Bienvenido "+ form.d.user
+        return render.index(form=cabecera)
 
 if __name__ == "__main__":
     app.run()
