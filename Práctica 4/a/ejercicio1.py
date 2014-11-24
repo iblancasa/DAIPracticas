d368cef0d97b6589b21032369d9b85da7a96f3c3
Israel Blancas <iblancasa@gmail.com>
2014-11-24 10:55:31 +0100

    Terminado ejercicio 1

--- Práctica 4/a/ejercicio1.py
+++ Práctica 4/a/ejercicio1.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import web
-
+import sys
+import urllib
 # etree sax parser
 from lxml import etree
+
+
 class ParseRssNews ():
-    def __init__ (self):
-        print ('---- Principio del archivo')
+    noticias=0
+    imagen=0
+    cantidadTermino = 0
+    termino=""
+
+    def __init__ (self,term):
+        self.termino=term
 
     def start (self, tag, attrib): # Etiquetas de inicio
-        print ('< %s>' % tag)
-        for k in attrib:
-            print (' %s = " %s"' % (k,attrib[k]))
+        if tag=="item":
+            self.noticias += 1
+        elif tag=="enclosure" and attrib['type']=="image/png":
+            self.imagen += 1
+            url=attrib['url']
+            filename = url[url.rfind("/") + 1:]
+            urllib.urlretrieve(url,filename)
 
     def end (self, tag): # Etiquetas de fin
-        print ('</ %s>' % tag)
+        return
 
     def data (self, data): # texto
-        print ('- %s-' % data)
+        if data.find(self.termino)>=0:
+            self.cantidadTermino+=1
 
     def close (self):
-        print ('---- Fin del archivo')
+        return
 
-parser = etree.XMLParser (target=ParseRssNews ())
+
+parseNews = ParseRssNews (sys.argv[1])
+parser = etree.XMLParser (target=parseNews)
 etree.parse ('rss.xml', parser)
 # rss.xml’ es un rss en
 # http://ep00.epimg.net/rss/elpais/portada.xml
+
+
+print "Nº imagenes " + str(parseNews.imagen)
+print "Nº noticias " + str(parseNews.noticias)
+print "Nº noticias con término interesante "+str(parseNews.cantidadTermino)
